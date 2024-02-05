2160f318b4d8dbed383201ca8f1b3edfbb098911
Tushar <tusharchemate07@gmail.com>
2024-02-06 01:29:37 +0530

    added connectionstring in app setting

---
+++ Data/StoreContext.cs
@@ -0,0 +1,14 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+        // represent products table in DB
+        public DbSet<Product> Products { get; set; }
+    }
+}
