6ec2e6a16d03c0991757b9b3d89a0dbdf5dc57f9
Tushar <tusharchemate07@gmail.com>
2024-02-06 22:37:16 +0530

    added async version of api in app

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -8,29 +9,27 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-
-        private readonly StoreContext context;
+        private readonly StoreContext _context;
 
         public ProductController(StoreContext context)
         {
-            this.context = context;
+            _context = context;
         }
 
 
         // Get Products list
         [HttpGet]
-        public ActionResult<List<Product>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = context.Products.ToList();
-            return Ok(products);
+            return await _context.Products.ToListAsync();
         }
 
         // Retrive individual product based on ID
 
         [HttpGet("{id}")]
-        public ActionResult<Product> GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return context.Products.Find(id);
+            return await _context.Products.FindAsync(id);
 
         }
 
