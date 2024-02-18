3c1a038c82bc990f804e65fc020189a0032250a6
Tushar <tusharchemate07@gmail.com>
2024-02-18 18:33:37 +0530

    added basket entity

--- Entities/BasketItem.cs
+++ Entities/BasketItem.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Entities
 {
+    [Table("BasketItems")]
     public class BasketItem
     {
         public int Id { get; set; }
@@ -15,6 +17,11 @@ namespace API.Entities
         public int ProductId { get; set; }
 
         public Product Product { get; set; }
+
+
+        public int BasketId { get; set; }
+
+        public Basket basket { get; set; }
 
     }
 }
