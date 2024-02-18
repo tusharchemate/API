d08edfda9765976c165ac8ffefff8973ef0c4f51
Tushar <tusharchemate07@gmail.com>
2024-02-18 19:51:38 +0530

    aaded table name in Basket

--- Entities/Basket.cs
+++ Entities/Basket.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Entities
 {
+    [Table("Baskets")]
     public class Basket
     {
 
